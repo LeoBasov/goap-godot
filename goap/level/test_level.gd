7c57771757dd6867aa9a1f108deffed334868b42
LeoBasov <40352679+LeoBasov@users.noreply.github.com>
2025-04-11 13:36:01 +0200

    reduced code

--- goap/level/test_level.gd
+++ goap/level/test_level.gd
@@ -1,4 +1,12 @@
 extends Node2D
 
 func _ready() -> void:
-	get_tree().call_group("objects", "snap", $TileMapLayer)
+	for object in get_tree().get_nodes_in_group("objects"):
+		snap(object)
+
+func snap(object):	
+	var grid_pos : Vector2 = $TileMapLayer.local_to_map(Vector2(object.position.x / object.sprite.scale.x, object.position.y / object.sprite.scale.y))
+	
+	object.position = $TileMapLayer.map_to_local(grid_pos)
+	object.position.x *= object.sprite.scale.x
+	object.position.y *= object.sprite.scale.y
--- goap/src/axe.gd
+++ goap/src/axe.gd
@@ -1,8 +1,6 @@
 extends Node2D
 
-func snap(grid):
-	var grid_pos : Vector2 = grid.local_to_map(Vector2(position.x / $Sprite2D.scale.x, position.y / $Sprite2D.scale.y))
-	
-	position = grid.map_to_local(grid_pos)
-	position.x *= $Sprite2D.scale.x
-	position.y *= $Sprite2D.scale.y
+var sprite : Sprite2D
+
+func _ready() -> void:
+	sprite = $Sprite2D
