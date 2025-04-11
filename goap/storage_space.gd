7cf3e2bb6ffbbec599503b3a34e0990a8dab1cb0
LeoBasov <40352679+LeoBasov@users.noreply.github.com>
2025-04-11 14:12:49 +0200

    refacotred objects

--- goap/storage_space.gd
+++ goap/storage_space.gd
@@ -1,20 +1,41 @@
 extends Node2D
 
+enum Options {EMPTY, AXE, STONE, WOOD}
+@export var current_object: Options
+
+var options_map : Dictionary
 var sprite_map : Dictionary
 var w : int = 16
 var h : int = 16
-var current_object = null
+var sprite : Sprite2D
+
+func _ready() -> void:
+	sprite = $Sprite2D
+	
+	check_state()
 
 func _init() -> void:
-	sprite_map["axe"] = Rect2(0, 0, w, h)
-	sprite_map["stone"] = Rect2(0, 0, w, h)
-	sprite_map["wood"] = Rect2(0, 0, w, h)
+	options_map["empty"] = Options.EMPTY
+	options_map["axe"] = Options.AXE
+	options_map["stone"] = Options.STONE
+	options_map["wood"] = Options.WOOD
+	
+	sprite_map[Options.AXE] = Rect2(672.0, 112.0, w, h)
+	sprite_map[Options.STONE] = Rect2(0, 0, w, h)
+	sprite_map[Options.WOOD] = Rect2(0, 0, w, h)
+	
+func check_state():
+	if current_object == Options.EMPTY:
+		$Sprite2D.hide()
+	else:
+		print($Sprite2D.region_rect)
+		$Sprite2D.region_rect = sprite_map[current_object]
+		$Sprite2D.show()
 
 func add(object_name):
-	current_object = object_name
-	$Sprite2D.region_rect = sprite_map[current_object]
-	$Sprite2D.show()
+	current_object = options_map[object_name]
+	check_state()
 
 func empty():
-	current_object = null
-	$Sprite2D.hide()
+	current_object = Options.EMPTY
+	check_state()
