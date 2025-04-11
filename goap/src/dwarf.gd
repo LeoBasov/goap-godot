896acc18d19e35ffbdcaa268306d7be98e9266e5
LeoBasov <40352679+LeoBasov@users.noreply.github.com>
2025-04-11 17:31:23 +0200

    added sybols for carrying

--- goap/src/dwarf.gd
+++ goap/src/dwarf.gd
@@ -2,10 +2,11 @@ extends Node2D
 
 var agent : Agent = Agent.new()
 var speed : float  = 200.0
-var has_axe = false
-var has_wood = false
+var has_axe: bool : set = set_axe
+var has_wood: bool : set = set_wood
 
 func _ready() -> void:
+	has_axe = false
 	agent.object = self
 
 func _process(delta: float) -> void:
@@ -13,3 +14,15 @@ func _process(delta: float) -> void:
 
 func moveto(target: Vector2, delta: float):
 	position += (target - position).normalized() * speed * delta
+
+func set_axe(state: bool):
+	if state:
+		$CarrySprite.set_object("axe")
+	else:
+		$CarrySprite.empty()
+		
+func set_wood(state: bool):
+	if state:
+		$CarrySprite.set_object("wood")
+	else:
+		$CarrySprite.empty()
