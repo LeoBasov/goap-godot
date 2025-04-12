3559f487ea17481b60db935cbda5aee7c4626e8c
Leo Basov <l.basov@sparc-industries.com>
2025-04-12 13:37:10 +0200

    added new tree structure

---
+++ goap/src/goap/goap_tree.gd
@@ -0,0 +1,12 @@
+class_name GoapTree extends Node
+
+var root : GoapTreeNode = null
+
+func build_tree(goal : Dictionary, actions : Array):
+	pass
+
+func add_node(parent, action):
+	pass
+
+func add_children(node: GoapTreeNode):
+	pass
---
+++ goap/src/goap/goap_tree_node.gd
@@ -0,0 +1,5 @@
+class_name GoapTreeNode extends Node
+
+var parent : GoapTreeNode = null
+var children : Array = []
+var action : Action = null
